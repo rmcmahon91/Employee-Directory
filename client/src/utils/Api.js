8f5feab3ea4ebb3cce5905e27e682f152c043b6d
Ryan McMahon <ryan.mcmahon4@gmail.com>
2020-05-05 21:06:12 -0500

    new upload

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export default {
     search: function () {
-        return axios.get("https://randomuser.me/api/?results=25")
+        return axios.get("https://randomuser.me/api/?results=150")
     }
 }
